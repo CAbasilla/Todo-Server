9a67f64dedb0a69cc25984d5af1c0d7ee1214d4e
CAbasilla <cbasilla@deloitte.com>
2025-02-10 20:54:29 +0800

    Update for the vercel work around.

--- src/app.ts
+++ src/app.ts
@@ -13,4 +13,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ message: err.message });
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log("Server ready on port 3000."));
