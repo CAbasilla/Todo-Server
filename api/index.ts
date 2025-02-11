03e46a80118dc60a6970afb49aa952fc79ceb5fb
CAbasilla <cbasilla@deloitte.com>
2025-02-11 09:29:59 +0800

    Update for the app setup.

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,27 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+// import { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default async (req: VercelRequest, res: VercelResponse) => {
-  return res.status(200).json({message: 'hello world'});
-};
+// export default async (req: VercelRequest, res: VercelResponse) => {
+//   return res.status(200).json({message: 'hello world'});
+// };
+
+import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
+
+import todoRoutes from '../src/routes/todos';
+import helloRoutes from '../src/routes/hello';
+
+const app = express();
+
+app.use(json());
+
+app.use('/', helloRoutes);
+
+app.use('/todos', todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message });
+});
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+export default app;
